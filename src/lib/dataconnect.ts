43bfaac9ff8818bc4dfda4176282835ff227c0a7
Jon Audiffred <jon.audiffred@gmail.com>
2025-07-21 18:11:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/dataconnect.ts
+++ src/lib/dataconnect.ts
@@ -2,8 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { connectDataConnectEmulator, getDataConnect } from 'firebase/data-connect';
 import { connectorConfig } from '@firebasegen/default-connector';
+import { getAnalytics } from "firebase/analytics";
 
-// TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -11,10 +11,14 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+if (typeof window !== 'undefined') {
+  getAnalytics(app);
+}
+
 export const dataConnect = getDataConnect(app, connectorConfig);
 
 if (process.env.NODE_ENV === 'development') {
